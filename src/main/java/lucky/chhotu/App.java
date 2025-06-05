35772a46a63948ae4516937d288c6a00ec2ecd28
LakshmiPrasad-hub <laxmipraasad12368@gmail.com>
2025-06-05 09:02:02 +0530

    aditya

---
+++ src/main/java/lucky/chhotu/App.java
@@ -0,0 +1,13 @@
+package lucky.chhotu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
